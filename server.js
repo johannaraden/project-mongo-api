988507a287506937306a4566dc1628bf0863971a
johannaraden <51335476+johannaraden@users.noreply.github.com>
2020-05-15 21:28:41 +0200

    added start page, added likes in data

--- server.js
+++ server.js
@@ -4,28 +4,12 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import data from './data/qa.json'
 
+
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/questions"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 
-// // Adding middleware for targeting server error for every request
-// app.use((req, res, next) => {
-//   if(mongoose.connection.readyState === 1) {
-//     next() 
-//   } else {
-//       res.status(503).json({error: 'Service not available'})
-//   }
-// })
-
-// User model
-const User = mongoose.model('User', {
-  text: {
-    type: Number
-  }
-})
-
-// Question model
 const Question = mongoose.model('Question', {
   id: {
     type: Number
@@ -33,16 +17,12 @@ const Question = mongoose.model('Question', {
   text: {
     type: String
   },
-  createdAt: {
-    type: Date, 
-    default: Date.now},
+  likes: {
+    type: Number
+  },
   answers: {
     id: Number,
     text: String
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
   }
 })
 
@@ -52,24 +32,8 @@ if (process.env.RESET_DATABASE) {
 
   const seedDatabase = async () => {
     await Question.deleteMany()
-    // await Answer.deleteMany()
-    const nowitall = new User({text: 'Now-it-all'}) 
-    // await User.deleteMany()
-    // await new Question({
-    // "id": 60, 
-    // "text": "I think my neighbor is stealing my wi-fi? What to do?", 
-    // "createdAt": "",
-    // "answers": [
-    //   {
-    //   "id": 1,
-    //   "text": "You sound a bit paranoid. Change the password and see if it gets hacked again."
-    //   },
-    //   {
-    //     "id": 2,
-    //     "text": "Annoying! Try to do Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    //     }}).save()
     //  Save the questions in qa.json to the database
-    await data.forEach((question) => new Question(question).save())
+    await data.forEach((question) => new Question(question).save());
   }
   seedDatabase()
 }
@@ -80,14 +44,27 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+// Adding middleware for targeting server error for every request
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable'})
+  }
+})  
 
 // My routes 
-// For the first route - creating regex to make it possible to search for a question including words like "playing" or "wi-fi"
+
+app.get('/', (req, res) => {
+  res.send(`<h2>Hello!</h2><h4>Possible endpoints for this API. </h4><ul><li>'/questions' shows all questions in the database</li><li>'/questions?query={'search word'}' gives the user the possibility to enter a search word to look for a question in a specific topic.</li><li>'/questions/:id' finds a specific question by id number</li><li>'/popular' returns the most popular questions based on number of likes</li></ul>`)
+})
+
+// For this route - creating regex to make it possible to search for a question including words like "playing" or "wi-fi"
 
 app.get('/questions', async (req, res) => {
   const { query } = req.query
-  const queryRegex = new RegExp(query)
-  const questions = await Question.find({text: queryRegex})
+  const queryRegex = new RegExp(query, 'i')
+  const questions = await Question.find({text: queryRegex}).populate('User')
   console.log(`Found ${questions.length} question(s)`)
   res.json(questions)
 })
@@ -96,7 +73,7 @@ app.get('/questions', async (req, res) => {
 
 app.get('/questions/:id', async (req, res) => {
   try {
-    const question = await Question.findById(req.params.id)
+    const question = await Question.findById(req.params.id) 
     if (question) {
       res.json(question)
     } else {
@@ -107,23 +84,14 @@ app.get('/questions/:id', async (req, res) => {
   }
 })
 
-// POST method to add a question 
-
-app.post('/questions', (req, res) => {
-  res.json({
-    response: 'This is a post request',
-    body: req.body
-  })
-})
-
-app.get('/qa', (req, res) => {
-  res.json(Question).populate('answer') 
-    if(question) {
-      res.json(question)
-    } else {
-      res.status(404).json({error: 'Sorry there is no question'})
-    }
-  })
+// app.get('/popular', (req, res) => {
+//   res.json(Question)
+//     if(question) {
+//       res.json(question).populate('User')
+//     } else {
+//       res.status(404).json({error: 'Sorry there is no question'})
+//     }
+//   })
 
 // Start the server
 app.listen(port, () => {
