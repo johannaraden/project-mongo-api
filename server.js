2cab2255b4fd8282cb229a846b47dc4af16725f3
johannaraden <51335476+johannaraden@users.noreply.github.com>
2020-05-18 22:28:03 +0200

    fixed error

--- server.js
+++ server.js
@@ -4,41 +4,62 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import data from './data/qa.json'
 
-
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/questions"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+// Reusable variable for error messages
+const ERR_NO_QUESTIONS = 'Sorry, could not find this question.'
 
-const Question = mongoose.model('Question', {
-  id: Number,
-  question: String,
-  likes: Number
+const User = mongoose.model('User', {
+  id: {
+    type: Number
+  },
+  userType: {
+    type: String
+  }
 })
 
-
-// if (process.env.RESET_DATABASE) {
-//   console.log('Resetting database..')
-
-//   const seedDatabase = async () => {
-//     await Question.deleteMany()
-//     //  Save the questions in qa.json to the database
-//     await data.forEach((question) => new Question(question).save());
-//     console.log('saving questions')
-//   }
-//   seedDatabase()
-// // }
+const Question = mongoose.model('Question', {
+  id: {
+    type: Number
+  },
+  question: {
+    type: String
+  },
+  likes: {
+    type: Number
+  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+})
+ 
 
 if (process.env.RESET_DB) {
   console.log('Resetting Database!')
   const seedDatabase = async () => {
     await Question.deleteMany({})
+    await User.deleteMany({})
     data.forEach((question) => {
       new Question(question).save()
     })
+    const rookie = await new User({
+      id: 1,
+      userType: 'Rookie'
+    }).save()
+    const experienced = await new User({
+      id: 2,
+      userType: 'Experienced'
+    }).save()
+    const knowItAll = await new User({
+      id: 3,
+      userType: 'Know-it-all'
+    }).save()
+    // const userArray = [knowItAll, experienced, rookie]
   }
   seedDatabase()
 }
+
+
 
 const port = process.env.PORT || 9000
 const app = express()
@@ -55,23 +76,35 @@ app.use((req, res, next) => {
   }
 })  
 
-// My routes 
+// My endpoints 
 
 app.get('/', (req, res) => {
-  res.send(`<h2>Hello!</h2><h4>Possible endpoints for this API. </h4><ul><li>'/questions' shows all questions in the database</li><li>'/questions?query={'search word'}' gives the user the possibility to enter a search word to look for a question in a specific topic.</li><li>'/questions/:id' finds a specific question by id number</li><li>'/popular' returns the most popular questions based on number of likes</li></ul>`)
+  res.send(`<h2>Hello!</h2><h4>Possible endpoints for this API. </h4><ul><li>'/questions' shows all questions in the database</li><li>'/questions?query={'search word'}' gives the user the possibility to enter a search word to look for a question in a specific topic.</li><li>'/questions/:id' finds a specific question by id number</li><li>'/popular' returns the most popular questions based on number of likes</li><li>'/nolikes' return the questions that have not yet recieved any likes.</li></ul>`)
 })
-
-// For this route - creating regex to make it possible to search for a question including words like "playing" or "wi-fi"
+ 
+// For this endpoint - creating regex to make it possible to search for a question including words like "playing" or "wi-fi"
 
 app.get('/questions', async (req, res) => {
   const { query } = req.query
   const queryRegex = new RegExp(query, 'i')
-  const questions = await Question.find({text: queryRegex})
+  // await User.count().exec((err, count) => {
+  //   let random = Math.floor(Math.random() * count)
+  //     User.findOne().skip(random).exec((err, result) => {
+  //       console.log(result) 
+  //     })
+  // })
+  const questions = await Question.find({question: queryRegex}).populate({
+    path: 'path',
+    options: {limit: myLimit}
+})
+  let myLimit = await Math.floor(Math.random() * (questions.length - 1) + 1)
+
+  console.log(myLimit)
   console.log(`Found ${questions.length} question(s)`)
   res.json(questions)
 })
 
-//Looking for a specific question.
+//Looking for a specific question by id.
 
 app.get('/questions/:id', async (req, res) => {
   try {
@@ -79,17 +112,28 @@ app.get('/questions/:id', async (req, res) => {
     if (question) {
       res.json(question)
     } else {
-      res.status(404).json({error: 'Sorry, this question does not exist.'})
+      res.status(404).json({error: ERR_NO_QUESTIONS})
   } 
   }catch (err) {
-    res.status(400).json({error: 'Sorry, there is no such question.'})
+    res.status(400).json({error: ERR_NO_QUESTIONS})
   }
 })
 
 // Top three page shows the three questions with the most likes 
 app.get('/popular', async(req, res) => {
-  const questions = await Question.sort({likes: 1}).slice(0, 2)
-  res.send(questions)
+  const popularQuestions = await Question.find({}).sort({likes: -1}).limit(3)
+  res.json(popularQuestions)
+  })
+
+  app.get('/nolikes', async(req, res) => {
+    const noLikesQuestions = await Question.find({likes: 0})
+    res.json(noLikesQuestions)
+  })
+
+  app.post('/question', async (req,res) => {
+    const question = new Question(req.body)
+    await question.save()
+    res.json(question)
   })
 
 // Start the server
